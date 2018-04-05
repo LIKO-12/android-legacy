1e03274510952b74298f25b339fe0dbc76552e61
RamiLego4Game <ramilego4game@gmail.com>
2018-04-05 13:22:12 +0200

    Enable x86 build

--- love/src/jni/Application.mk
+++ love/src/jni/Application.mk
@@ -2,7 +2,7 @@
 # See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
 # APP_STL := stlport_static 
 APP_STL := c++_shared
-APP_ABI := armeabi-v7a
+APP_ABI := armeabi-v7a x86
 APP_CPPFLAGS := -frtti
 APP_LDFLAGS := -llog -landroid -lz
 APP_PLATFORM := 10
