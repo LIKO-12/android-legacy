6f11f986fe840f0174ecf0db077ab41d20f159e6
RamiLego4Game <ramilego4game@gmail.com>
2018-04-05 15:19:40 +0200

    Fix libcurl

--- love/src/jni/libcurl/Android.mk
+++ love/src/jni/libcurl/Android.mk
@@ -1,18 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-
-LOCAL_MODULE := libcurl_static
-LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libcurl.a
+LOCAL_MODULE := libcurl-prebuilt
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libcurl.so
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
-
-include $(PREBUILT_STATIC_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := libcurl
-LOCAL_SHARED_LIBRARIES := openssl opencrypto
-LOCAL_STATIC_LIBRARIES := libcurl_static
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
-
-#include $(BUILD_SHARED_LIBRARY)
+include $(PREBUILT_SHARED_LIBRARY)
